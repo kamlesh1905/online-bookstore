ec068b7bb9ba39a7096f044058afe916eed90d90
Kamlesh merugu <Kamlesh merugu@DESKTOP-8HGJPAF>
2020-04-18 19:52:36 +0800

    created angular project and completed the feature 1

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,12 @@
+export class Book 
+{
+    sku:string;
+    name:string;
+    description:string;
+    unitPrice:number;
+    imageUrl:string;
+    active:boolean;
+    unitsInStock:number;
+    creationOn:Date;
+    updatedOn:Date;
+}
